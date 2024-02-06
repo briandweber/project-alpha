20952480b15436b7328077be0eeec24a5815b80d
Brian <briandweber@gmail.com>
2024-02-06 17:47:49 -0600

    Feature 8 complete

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,7 @@ from projects.views import list_projects, show_project, create_project
 
 
 urlpatterns = [
+    path("", list_projects, name="list_projects"),
     path("", list_projects, name="home"),
     path("<int:id>/", show_project, name="show_project"),
     path("create/", create_project, name="create_project"),
